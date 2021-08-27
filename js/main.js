ffb78e9786aae3d20d3ad26d75f108e216053bb4
jaeyong4536 <jaeyong4536@gmail.com>
2021-08-27 20:49:08 +0900

    헤더 드랍다운 메뉴 & 배지 추가
    index.html
    : gsap 라이브러리 연결.
      lodash 라이브러리 연결.
      드랍다운 메뉴 추가.
      배지 추가.
    
    css/main.css
    : 드랍다운 스타일 추가.
      배지 스타일 추가.
      inner클래스 공통 스타일 추가.
    
    js/main.js
    : 검색 관련 함수명 리팩토링.
      배지 표시/비표시의 스크롤 이벤트 추가.

--- js/main.js
+++ js/main.js
@@ -1,24 +1,41 @@
-const searchEl = document.querySelector(".search");
+const headerEl = document.querySelector("header");
+const searchEl = headerEl.querySelector(".search");
 const searchInputEl = searchEl.querySelector("input");
-
-function focused() {
-  searchEl.classList.add("focused");
-  searchInputEl.setAttribute("placeholder", "통합검색");
-}
-
-function blurred() {
-  searchEl.classList.remove("focused");
-  searchInputEl.setAttribute("placeholder", "");
-}
+const badgesEl = headerEl.querySelector(".badges");
 
 function openSearchInputEl() {
   searchInputEl.focus();
 }
 
+function searchfocused() {
+  searchEl.classList.add("focused");
+  searchInputEl.setAttribute("placeholder", "통합검색");
+}
+
+function searchblurred() {
+  searchEl.classList.remove("focused");
+  searchInputEl.setAttribute("placeholder", "");
+}
+
+function controlBadges() {
+  if (window.scrollY > 500) {
+    gsap.to(badgesEl, .4, {
+      opacity: 0,
+      display: "none"
+    });
+  } else {
+    gsap.to(badgesEl, .3, {
+      opacity: 1,
+      display: "block"
+    });
+  }
+}
+
 function init() {
   searchEl.addEventListener("click", openSearchInputEl);
-  searchInputEl.addEventListener("focus", focused);
-  searchInputEl.addEventListener("blur", blurred);
+  searchInputEl.addEventListener("focus", searchfocused);
+  searchInputEl.addEventListener("blur", searchblurred);
+  document.addEventListener("scroll", _.throttle(controlBadges, 300))
 }
 
 init();
